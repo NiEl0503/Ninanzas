d4b8ad2a4f0deae899ca7dfb6bf862b3355139f6
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-06-11 12:36:00 -0300

    feat: configuración de las urls para mostrar las vistas y ajustes en settings

---
+++ ninanzas/ninanzas_app/urls.py
@@ -0,0 +1,14 @@
+"""Modulo que maneja las rutas para que las vistas sean accesibles"""
+from django.views.generic.base import RedirectView
+from django.urls import path
+from ninanzas_app.views import (UserLoginAPIView, UserRegistrationAPIView,
+                                CategoryAPIView, TransactionAPIView, BudgetAPIView)
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='/login/', permanent=True)),
+    path('login/', UserLoginAPIView.as_view(), name='login'),
+    path('register/', UserRegistrationAPIView.as_view(), name='register'),
+    path('categories/', CategoryAPIView.as_view(), name='categories'),
+    path('transactions/', TransactionAPIView.as_view(), name='transactions'),
+    path('budgets/', BudgetAPIView.as_view(), name='budgets'),
+]
