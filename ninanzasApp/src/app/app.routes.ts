88c992df5ca82b1c85a2397821de6f3ce8cd59e6
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-06-14 12:58:47 -0300

    feat: creación del servicio api para manejar las llamadas HTTP, configuración de las rutas y modulos en angular

--- ninanzasApp/src/app/app.routes.ts
+++ ninanzasApp/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { BudgetsComponent } from './components/budgets/budgets.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'budgets', component: BudgetsComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
