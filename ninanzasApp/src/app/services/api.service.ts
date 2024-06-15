d73ed5b62737a2937c3d85e9f75f561dac380b9f
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-06-15 18:30:22 -0300

    feat: configuarcion de api.service.ts para manejar las llamadas HTTP

--- ninanzasApp/src/app/services/api.service.ts
+++ ninanzasApp/src/app/services/api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl = 'http://127.0.0.1:8000';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login/`, data);
+  }
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register/`, data);
+  }
+
+  getCategories(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/categories/`);
+  }
+
+  getTransactions(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/transactions/`);
+  }
+
+  getBudgets(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/budgets/`);
+  }
 }
+
