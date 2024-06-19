5c5bfd6afc422f51f37e4acf2c645a8b526c511f
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-06-19 14:26:33 -0300

    fix: corrigiendo el error: NullInjectorError: No provider for _HttpClient --> cambi la baseurl en apiservice, coloque JwtInterceptor como injectable

--- ninanzasApp/src/app/services/api.service.ts
+++ ninanzasApp/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'httpClient://127.0.0.1:8000';
+  private baseUrl = 'http://127.0.0.1:8000';
 
   constructor(private httpClient: HttpClient) { }
 
