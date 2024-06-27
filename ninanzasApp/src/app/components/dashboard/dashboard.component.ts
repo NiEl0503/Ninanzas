5103b5a4ce3ef7aeee7b8bbc77b947fff86e32fc
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-06-27 15:51:13 -0300

    fix: corregida la vista de dashboard (se incluyo categories, total income, total expense y total savings) feat: se anadio la logica y el html al dashboard

--- ninanzasApp/src/app/components/dashboard/dashboard.component.ts
+++ ninanzasApp/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  totalIncome: number = 0;
+  totalExpense: number = 0;
+  totalSavings: number = 0;
+  transactions: any[] = [];
+  budgets: any[] = [];
+  categories: any[] = [];
+  errorMessage: string = '';
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getDashboard().subscribe(response => {
+      this.totalIncome = response.total_income;
+      this.totalExpense = response.total_expense;
+      this.totalSavings = response.total_savings;
+      this.transactions = response.transactions;
+      this.budgets = response.budgets;
+      this.categories = response.categories;
+    }, error => {
+      console.error(error);
+      this.errorMessage = 'Error al cargar el dashboard: problema del servidor o datos incorrectos 😱';
+    });
+  }
 }
